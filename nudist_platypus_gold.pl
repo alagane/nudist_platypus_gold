5eee70a9481ceea00a6666b76528f67621bd95fe
Victor Adam <victor.adam@cofelyineo-gdfsuez.com>
2015-08-25 18:15:30 +0200

    Bugfixes for the Perl version
    * Hide the cursor
    * Do not leave stray characters on the left border
    * Avoid blinking near the top border
    * 50 characters shorter!

--- nudist_platypus_gold.pl
+++ nudist_platypus_gold.pl
@@ -1,4 +1,4 @@
-sub'r{$==rand pop}$|=print"\e[2J";r$l=`tput lines`-2,s/^$/"\e[$=;".r($c
-=`tput cols`-2).f.' Oo*... '=~s!\S\K!\e[B\b !gr/e/s/\d++\B/($a=$&-1)+($
-==!$a+r$a%$c?3:2)/e/s/(?<=(.)...B.)./$1/g/s/f\K./chr 32>>$=/e/s/\d+/($&
-||$l)-1/e,select$,,$,,$,,.009*print$&?$_:"\e[f"."\e[K\n"x6while*_=r.r+9
+sub'r{$-=!$=+rand pop}print"\e[2J\e[?25l";
+$_||="\e[1;${\r$c=`tput cols`-2}fO".'o*... '=~s/|/\e[B\b /gr,
+s/\d+/$\=y!Oo*.! !rx!($==$&-1);$|--?$=+r$=%$c?3:2:$=*print||`tput lines`-1
+/ge,s/(?<=f|(.))...B.\K./$1||chr 32>>$-/ge while*_=a.r+42
