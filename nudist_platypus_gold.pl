7c4796c640baaf66dadfcfcb723edd7f98895a9b
Victor Adam <victor.adam@cofelyineo-gdfsuez.com>
2015-08-26 18:24:37 +0200

    Shaved two characters!

--- nudist_platypus_gold.pl
+++ nudist_platypus_gold.pl
@@ -1,3 +1,3 @@
-sub'v{$==!$-+rand pop}!s/\d+/$|--?($-=$&-1)+v$-%$c?3:2:$&-print||`stty size`-
-print???"\e[?25l\e[2J":y!Oo*.! !r/ge?$_="\e[1;".v($c=`tput cols`-2).fO.'o*... '
-=~s/|/\e[B\b /gr:map//,1..s/(?<=f|(.))...B.\K./$1||chr 32>>$=/ge**5while*_=v-42
+map{sub'v{$==!$-+rand pop}}!s/\d+/$|--?($-=$&-1)+v$-%$c?3:2:$&-print||`stty size`
+-print???"\e[?25l\e[2J":y!Oo*.! !r/ge?$_="\e[1;".v($c=`tput cols`-2).fO.'o*... '
+=~s/|/\e[B\b /gr:s/(?<=f|(.))...B.\K./$1||chr 32>>$=/ge..4E4while*_=v-42
